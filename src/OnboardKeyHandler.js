16edcebc6d6273836a20dc7b5f0963c44cf77eb5
Jaroslav Benc <jaroslav.benc@gmail.com>
2017-06-22 08:48:44 +0200

    Prevent default when bound keys are triggered

--- src/OnboardKeyHandler.js
+++ src/OnboardKeyHandler.js
@@ -12,7 +12,7 @@ export default class OnboardKeyHandler {
 		 */
 		this._options = options;
 		this._model = model;
-		
+
 		this._onStartBinding = this._model.on("start", this._onStart.bind(this));
 		this._onStopBinding = this._model.on("stop", this._onStop.bind(this));
 	}
@@ -37,7 +37,11 @@ export default class OnboardKeyHandler {
 			this._model.hasNext()?this._model.next():this._model.stop();
 		} else if (d3.event.key == "Escape"){
 			this._model.stop();
+		} else {
+			return;
 		}
+
+		d3.event.preventDefault();
 	}
 
 	/**
