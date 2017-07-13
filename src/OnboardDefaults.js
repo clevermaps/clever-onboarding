65d515e1ae55753df79c42f49d3a05914cc76694
Jaroslav Benc <jaroslav.benc@gmail.com>
2017-07-13 13:33:26 +0200

    Fix positioning changes

--- src/OnboardDefaults.js
+++ src/OnboardDefaults.js
@@ -2,13 +2,13 @@
  * @public
  * Default fill color
  */
-export const FILL_COLOR = "#000000";
+export const FILL_COLOR = "#343A3D";
 
 /**
  * @public
  * Default fill opacity
  */
-export const FILL_OPACITY = 0.25;
+export const FILL_OPACITY = 0.5;
 
 /**
  * @public
